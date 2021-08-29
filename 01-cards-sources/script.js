30fb4d4eb712fd3d320c9d01969cb904d52d8a74
Pavlechko <pavlechko.serhii@gmail.com>
2021-08-30 01:12:43 +0300

    end the practice

---
+++ 01-cards-sources/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach((slide) => {
+    slide.addEventListener("click", () => {
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+        
+        slide.classList.add("active");
+    });
+});
