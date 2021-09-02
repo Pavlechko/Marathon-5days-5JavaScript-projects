927c464400769cc058d64d6c4ab3cbcb40215185
Pavlechko <pavlechko.serhii@gmail.com>
2021-09-03 00:32:20 +0300

    end the practice day five

---
+++ 05-aim-game-sources/script.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const startBtn = document.querySelector("#start"),
+      screens = document.querySelectorAll(".screen"),
+      timeList = document.querySelector("#time-list"),
+      timeEl = document.querySelector("#time"),
+      board = document.querySelector(".board");
+
+let time = 0,
+    score = 0;
+
+startBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    screens[0].classList.add("up");
+});
+
+timeList.addEventListener("click", (event) => {
+    if (event.target.classList.contains("time-btn")) {
+        time = parseInt(event.target.getAttribute("data-time"));
+        screens[1].classList.add("up");
+        startGame();
+    }
+});
+
+board.addEventListener("click", (event) => {
+    if (event.target.classList.contains("circle")) {
+        score++;
+        event.target.remove();
+        createRandomCircle();
+    }
+    console.log("err");
+})
+
+function startGame() {
+    setInterval(descreaseTime, 1000);
+    createRandomCircle();
+    setTime(time);
+}
+
+function descreaseTime() {
+    if (time === 0) {
+        finishGame();
+    } else {
+        let current = --time;
+
+        if (current < 10) {
+            current = `0${current}`
+        }
+        setTime(current);
+    }        
+}
+
+function setTime(value) {
+    timeEl.innerHTML = `00:${value}`;
+}
+
+function finishGame() {
+    timeEl.parentNode.classList.add("hide");
+    board.innerHTML = `<h1>Score: <span class="primary">${score}</span></h1>`;
+}
+
+function createRandomCircle() {
+    const circle = document.createElement("div"),
+          size = getRandomNumber(10, 60),
+          {width, height} = board.getBoundingClientRect(),
+          x = getRandomNumber(0, width - size),
+          y = getRandomNumber(0, height - size);
+
+    circle.classList.add("circle");
+    circle.style.width = `${size}px`;
+    circle.style.height = `${size}px`;
+    circle.style.top = `${y}px`;
+    circle.style.left = `${x}px`;
+
+    board.append(circle);
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
